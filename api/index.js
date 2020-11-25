7cf80866597f1f4e5bc6bf1016f527f90580833e
nolan <nolan@e-major.fr>
2020-11-25 19:07:23 +0100

    mise en place de swagger

--- api/index.js
+++ api/index.js
@@ -13,6 +13,17 @@ server.use(bodyParser.urlencoded());
 server.use(bodyParser.json());
 
 
+const cors = require('cors');
+const corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+server.use(cors(corsOptions));
+
+
+server.use(express.static('swagger'));
+
+
 const memberRoute = require('./routes/memberRoute');
 memberRoute(server);
 
