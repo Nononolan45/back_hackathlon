016765f0ede904a831ee75539c7d7ea5b5a0c8cb
nolan <nolan@e-major.fr>
2020-11-25 23:18:53 +0100

    add item users to swagger

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,7 +1,7 @@
 module.exports = (server) => {
     const userController = require('../controllers/userController');
 
-    server.route('/users/:school_id/users')
+    server.route('/schools/:school_id/users')
           .get(userController.list_all_users)
           .post(userController.create_an_user);
 
