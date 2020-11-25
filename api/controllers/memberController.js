01e290d89253e74e6ae39a64d5f4cd93a4199a78
nolan <nolan@e-major.fr>
2020-11-25 16:01:17 +0100

    controller member and middleware member

--- api/controllers/memberController.js
+++ api/controllers/memberController.js
@@ -5,33 +5,42 @@ const routeProvider = require('../providers/route');
 
 
 exports.list_all_members = (req, res) => {
-    Member.find({ project_id: req.params.project_id }, (error, members) => {
-        if (error) {
-            routeProvider.generateError(500, 'Erreur serveur', res);
-        }
-        else {
-            routeProvider.generateSuccess(200, members, res);
-        }
-    });
+    Member.find(
+        { project_id: req.params.project_id },
+        (error, members) => {
+            if (error) {
+                routeProvider.generateError(500, 'Erreur serveur', res);
+            }
+            else {
+                routeProvider.generateSuccess(200, members, res);
+            }
+        });
 }
 
 exports.create_a_member = (req, res) => {
-    Project.findById(req.params.project_id, (error, project) => {
-        if (error) {
-            routeProvider.generateError(500, 'Erreur serveur', res);
-        }
-        else {
-            let new_member = new Member({ project_id: req.params.project_id, ...req.body });
-            new_member.save((error, member) => {
-                if (error) {
-                    routeProvider.generateError(500, 'Erreur serveur', res);
-                }
-                else {
-                    routeProvider.generateSuccess(201, member, res);
-                                    }
-            });
-        }
-    });
+    Project.findById(
+        req.params.project_id,
+        (error) => {
+            if (error) {
+                routeProvider.generateError(500, 'Erreur serveur', res);
+            }
+            else {
+                let new_member = new Member(
+                    {
+                        project_id: req.params.project_id,
+                        ...req.body
+                    });
+                new_member.save(
+                    (error, member) => {
+                        if (error) {
+                            routeProvider.generateError(500, 'Erreur serveur', res);
+                        }
+                        else {
+                            routeProvider.generateSuccess(201, member, res);
+                        }
+                    });
+            }
+        });
 
 
 }
@@ -39,37 +48,45 @@ exports.create_a_member = (req, res) => {
 exports.get_a_member = (req, res) => {
 
 
-    Member.findById(req.params.member_id, (error, member) => {
-        if (error) {
-            routeProvider.generateError(500, 'Erreur serveur', res);
-        }
-        else {
-           routeProvider.generateSuccess(200,member, res);
-        }
-    });
+    Member.findById(
+        req.params.member_id,
+        (error, member) => {
+            if (error) {
+                routeProvider.generateError(500, 'Erreur serveur', res);
+            }
+            else {
+                routeProvider.generateSuccess(200, member, res);
+            }
+        });
 }
 
 exports.update_a_member = (req, res) => {
-    Member.findByIdAndUpdate(req.params.member_id, req.body, { new: true, useFindAndModify: false }, (error, member) => {
-        if (error) {
-            routeProvider.generateError(500, 'Erreur serveur', res);
-        }
-        else {
-            routeProvider.generateSuccess(200, member, res);
-            res.status(200);
-            res.json(member);
-        }
-    });
+    Member.findByIdAndUpdate(
+        req.params.member_id,
+        req.body,
+        {
+            new: true,
+            useFindAndModify: false
+        },
+        (error, member) => {
+            if (error) {
+                routeProvider.generateError(500, 'Erreur serveur', res);
+            }
+            else {
+                routeProvider.generateSuccess(200, member, res);
+            }
+        });
 }
 
 exports.delete_a_member = (req, res) => {
-    Member.findByIdAndRemove(req.params.member_id, (error) => {
-        if (error) {
-            routeProvider.generateError(500, 'Erreur serveur', res);
-        }
-        else {
-            res.status(200);
-            routeProvider.generateSuccess(200, { message: 'Membre supprimé' }, res);
-        }
-    })
+    Member.findByIdAndRemove(
+        req.params.member_id,
+        (error) => {
+            if (error) {
+                routeProvider.generateError(500, 'Erreur serveur', res);
+            }
+            else {
+                routeProvider.generateSuccess(200, { message: 'Membre supprimé' }, res);
+            }
+        })
 }
