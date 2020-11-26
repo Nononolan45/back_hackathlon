8f1d9d72439edde7a0e40bb1c819e10af8011e1e
El hadji BADIAGA <embadiaga@gmail.com>
2020-11-26 11:15:52 +0100

    login creation

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,30 +1,31 @@
 const User = require('../models/userModel');
 const routeProvider = require('../providers/route');
-
+const jwt = require('jsonwebtoken');
+const bcryptProvider = require('../providers/bcrypt');
 
 exports.list_all_users = (req, res) => {
-    User.find({school_id: req.params.school_id}, (error, users) => {
-      if(error){
-        routeProvider.generateError(500, 'Erreur serveur', res);
-      }
-      else{
-        routeProvider.generateSuccess(200, users, res);
-      }
+    User.find({ school_id: req.params.school_id }, (error, users) => {
+        if (error) {
+            routeProvider.generateError(500, 'Erreur serveur', res);
+        }
+        else {
+            routeProvider.generateSuccess(200, users, res);
+        }
     })
-  }
-  
+}
+
 
 exports.create_an_user = (req, res) => {
     let new_user = new User(req.body);
     new_user.school_id = req.params.school_id;
 
     new_user.save((error, user) => {
-        if(error){
+        if (error) {
             routeProvider.generateError(500, 'Erreur serveur', res);
-          }
-          else{
+        }
+        else {
             routeProvider.generateSuccess(201, user, res);
-          }
+        }
     });
 }
 
@@ -32,9 +33,9 @@ exports.create_an_user = (req, res) => {
 
 exports.get_an_user = (req, res) => {
     User.findById(req.params.user_id, (error, user) => {
-        if(error){
+        if (error) {
             routeProvider.generateError(500, 'Erreur serveur', res);
-        }else{
+        } else {
             routeProvider.generateSuccess(200, user, res);
         }
     });
@@ -42,9 +43,9 @@ exports.get_an_user = (req, res) => {
 
 exports.update_an_user = (req, res) => {
     User.findByIdAndUpdate(req.params.user_id, req.body, { new: true, useFindAndModify: false }, (error, user) => {
-        if(error){
+        if (error) {
             routeProvider.generateError(500, 'Erreur serveur', res);
-        }else{
+        } else {
             routeProvider.generateSuccess(200, user, res);
         }
     });
@@ -52,10 +53,41 @@ exports.update_an_user = (req, res) => {
 
 exports.delete_an_user = (req, res) => {
     User.findByIdAndRemove(req.params.user_id, (error) => {
-        if(error){
+        if (error) {
             routeProvider.generateError(500, 'Erreur serveur', res);
-        }else{
+        } else {
             routeProvider.generateSuccess(200, 'utilisateur supprimé', res);
         }
     })
 }
+
+exports.login_an_user = (req, res) => {
+    User.findOne({ email: req.body.email }, (error, user) => {
+        {
+            if (error) {
+                routeProvider.generateError(500, 'Erreur serveur', res);
+            }
+            else if (user) {
+                if (bcryptProvider.verifyPassword(req.body.password, user.password)) {
+                    jwt.sign({ email: user.email, name: user.name, user_id: user.id, school_id: user.school_id },
+                        process.env.JWT_SECRET,
+                        { expiresIn: '30 days' },
+                        (error, token) => {
+                            if (error) {
+                                routeProvider.generateError(500, "Mot de passe ou email erroné", res);
+                            } else {
+                                routeProvider.generateSuccess(200, { token }, res);
+
+                            }
+                        });
+                }
+                else {
+                    routeProvider.generateError(500, "Mot de passe ou email erroné", res);
+                }
+            }
+            else {
+                routeProvider.generateError(500, 'Erreur serveur', res);
+            }
+        }
+    });
+}  
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,7 +1,7 @@
 module.exports = (server) => {
     const userController = require('../controllers/userController');
 
-    server.route('/users/:school_id/users')
+    server.route('/schools/:school_id/users')
           .get(userController.list_all_users)
           .post(userController.create_an_user);
 
@@ -9,4 +9,6 @@ module.exports = (server) => {
         .get(userController.get_an_user)
         .put(userController.update_an_user)
         .delete(userController.delete_an_user);
+    
+    server.route('/users/login').post(userController.login_an_user);
 }
