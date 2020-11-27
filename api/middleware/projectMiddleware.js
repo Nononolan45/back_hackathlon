7c500a3ae29a81703c22a08bf8cd0f0a1b2123ca
nolan <nolan@e-major.fr>
2020-11-27 17:10:59 +0100

    add middleware to veridy user is attach to school, and verify token

---
+++ api/middleware/projectMiddleware.js
@@ -0,0 +1,29 @@
+const routeProvider = require('../providers/route');
+const jwt = require('jsonwebtoken');
+const Project = require('../models/projectModel');
+
+
+exports.verifyUsserIsAttachedToSchool = (req, res, next) => {
+    let token = req.headers['authorization'];
+
+    const decoded = jwt.decode(token);
+    Project.findById(
+        req.params.project_id,
+        (error, project) => 
+        {
+            if (error) 
+            {
+                routeProvider.generateError(500, 'Erreur serveur', res);
+            }
+            else if(project.school_id === decoded.school_id) 
+                {
+                    next();
+                }
+                else 
+                {
+                    routeProvider.generateError(403, 'Accès interdit', res);
+
+                }
+            
+        });
+}
