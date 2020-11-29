719ba61e353328b93132a0ab3f11f3656120ed23
nolan <nolan@e-major.fr>
2020-11-29 03:10:06 +0100

    check middleware OK

--- api/middleware/jwtMiddleware.js
+++ api/middleware/jwtMiddleware.js
@@ -1,31 +1,35 @@
 const jwt = require('jsonwebtoken');
 const routeProvider = require('../providers/route');
 
-
+// if JWT_SECRET in.env is empty 
 const JWT_SECRET = process.env.JWT_SECRET|| '123456789';
 
 
-exports.verify_token = (req, res, next) => {
-    console.log('on passe ici');
+exports.verify_token = (req, res, next) => 
+{
+    // get token in request
     let token = req.headers['authorization'];
-    console.log(req.headers);
-    if(typeof token != 'undefined'){
-        jwt.verify(token, JWT_SECRET, (error) => {
-            if(error){
-                routeProvider.generateError(403, 'Accès interdit', res);
-            }else{
+
+    // check type token
+    if(typeof token != 'undefined')
+    {   
+        // verify token with jsonwebtoken
+        jwt.verify(token, JWT_SECRET, (error) => 
+        {
+            if(error)
+            {
+                routeProvider.generateError(401, 'Accès interdit', res);
+            }
+            else
+            {
+                // authorize to continue
                 next();
             }
         });
     }
-    else {
+    else 
+    {
         routeProvider.generateError(403, 'Accès interdit', res);
     }
 }
 
-exports.addNameToContent = (req, res, next) => {
-    let token = req.headers['authorization'];
-    let decodeToken = jwt.decode(token);
-    req.body.name = decodeToken.name;
-    next();
-}
--- api/middleware/projectMiddleware.js
+++ api/middleware/projectMiddleware.js
@@ -4,25 +4,27 @@ const Project = require('../models/projectModel');
 
 
 exports.verifyUsserIsAttachedToSchool = (req, res, next) => {
+    // verify user is link to project by school_id
+
     let token = req.headers['authorization'];
-    console.log(token);
     const decoded = jwt.decode(token);
-    console.log(decoded)
+
     Project.findById(
         req.params.project_id,
         (error, project) => 
         {
-            console.log(project);
             if (error) 
             {
                 routeProvider.generateError(500, 'Erreur serveur', res);
             }
             else if(project) 
                 {   
-                    if(project.school_id === decoded.school_id){
+                    if(project.school_id === decoded.school_id)
+                    {
                         next();
                     } 
-                    else{
+                    else
+                    {
                         routeProvider.generateError(500, 'Erreur serveur', res);
 
                     }
@@ -32,6 +34,5 @@ exports.verifyUsserIsAttachedToSchool = (req, res, next) => {
                     routeProvider.generateError(403, 'Accès interdit', res);
 
                 }
-            
         });
 }
