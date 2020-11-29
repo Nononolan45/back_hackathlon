16e419ab93ee847aaa8f5787d44c160cb21e63fd
nolan <nolan@e-major.fr>
2020-11-29 03:05:22 +0100

    chek all controller OK

--- api/controllers/projectController.js
+++ api/controllers/projectController.js
@@ -1,105 +1,140 @@
 const School = require('../models/schoolModel');
 const Project = require('../models/projectModel');
 const Member = require('../models/memberModel');
-
-
 const routeProvider = require('../providers/route');
 
+// Generate methods fro CRUD project
 
-exports.list_all_projects = (req, res) => {
+
+exports.list_all_projects = (req, res) => 
+{
     Project.find(
-        { school_id: req.params.school_id },
-        (error, schools) => {
-            if (error) {
+        { 
+            school_id: req.params.school_id 
+        },
+        (error, schools) => 
+        {
+            if (error) 
+            {
                 routeProvider.generateError(500, 'Erreur serveur', res);
             }
-            else {
+            else 
+            {
                 routeProvider.generateSuccess(200, schools, res);
             }
         });
 }
 
-exports.list_all_projects_always_available = (req, res) => {
+exports.list_all_projects_always_available = (req, res) => 
+{
+    // Get all projects when member limit is < 5
     Project.find(
-        { school_id: req.params.school_id },
-        (error, projects) => {
-            if (error) {
+        { 
+            school_id: req.params.school_id 
+        },
+        (error, projects) => 
+        {
+            if (error) 
+            {
                 routeProvider.generateError(500, 'Erreur serveur', res);
             }
-            else {
+            else 
+            {
                 let projectsFilter = [];
-                if (projects.length > 0) {
+                if (projects.length > 0) 
+                {
                     let i = 0;
-                    projects.forEach((project) => {
+                    projects.forEach((project) => 
+                    {
+                        //  for each project, get list member
                         Member.find(
-                            { project_id: project.id },
-                            (error, members) => {
+                            { 
+                                project_id: project.id 
+                            },
+                            (error, members) => 
+                            {
                                 i++;
-                                if (error) {
+                                if (error) 
+                                {
                                     routeProvider.generateError(500, 'Erreur serveur', res);
                                 }
-                                else if (members.length != 5) {
+                                else if (members.length != 5) 
+                                {
+                                    // limit member < 5, is OK
+                                    // push project in tab result 
                                     projectsFilter.push(project);
-                                    if (i === projects.length) {
+
+                                    if (i === projects.length) 
+                                    {
+                                        // check all member is test
                                         routeProvider.generateSuccess(200, projectsFilter, res);
 
                                     }
                                 }
                             });
                     });
-                } else {
+                } 
+                else 
+                {
                     routeProvider.generateSuccess(200, [], res);
 
                 }
-
-
             }
         });
 }
 
 
-exports.create_a_project = (req, res) => {
+exports.create_a_project = (req, res) => 
+{
     School.findById(
         req.params.school_id,
         (error) => {
-            if (error) {
+            if (error) 
+            {
                 routeProvider.generateError(500, 'Erreur serveur', res);
             }
-            else {
+            else 
+            {
                 let new_project = new Project(
                     {
                         school_id: req.params.school_id,
                         ...req.body
                     }
                 );
-                new_project.save((error, project) => {
-                    if (error) {
+                new_project.save((error, project) => 
+                {
+                    if (error) 
+                    {
                         routeProvider.generateError(500, 'Erreur serveur', res);
                     }
-                    else {
+                    else 
+                    {
                         routeProvider.generateSuccess(201, project, res);
                     }
                 });
             }
         });
-
-
 }
 
-exports.get_a_project = (req, res) => {
+exports.get_a_project = (req, res) => 
+{
     Project.findById(
         req.params.project_id,
-        (error, project) => {
-            if (error) {
+        (error, project) => 
+        {
+            if (error) 
+            {
                 routeProvider.generateError(500, 'Erreur serveur', res);
             }
-            else {
+            else 
+            {
                 routeProvider.generateSuccess(200, project, res);
             }
         });
 }
 
-exports.update_a_project = (req, res) => {
+exports.update_a_project = (req, res) => 
+{
     Project.findByIdAndUpdate(
         req.params.project_id,
         req.body,
@@ -107,24 +142,31 @@ exports.update_a_project = (req, res) => {
             new: true,
             useFindAndModify: false
         },
-        (error, project) => {
-            if (error) {
+        (error, project) => 
+        {
+            if (error) 
+            {
                 routeProvider.generateError(500, 'Erreur serveur', res);
             }
-            else {
+            else 
+            {
                 routeProvider.generateSuccess(200, project, res);
             }
         });
 }
 
-exports.delete_a_project = (req, res) => {
+exports.delete_a_project = (req, res) => 
+{
     Project.findByIdAndRemove(
         req.params.project_id,
-        (error) => {
-            if (error) {
+        (error) => 
+        {
+            if (error) 
+            {
                 routeProvider.generateError(500, 'Erreur serveur', res);
             }
-            else {
+            else 
+            {
                 routeProvider.generateSuccess(200, { message: 'Projet supprimé' }, res);
             }
         })
